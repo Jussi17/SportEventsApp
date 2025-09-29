ff98a37017ee55170977b126b14c44cf33a0aa11
Jussi17 <jus.jj@hotmail.com>
2025-09-29 11:25:06 +0300

    Aloitusprojekti: .NET MAUI app pohja

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace SportEventsApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
