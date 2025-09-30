a5e53fda7e3663af31191c3794f02f58ec54d88e
Jussi17 <jus.jj@hotmail.com>
2025-09-30 11:40:32 +0300

    Teema
    Yksinkertainen tumma/vaalea teeman valinta hampurilaisvalikossa

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -6,4 +6,36 @@ public partial class AppShell : Shell
 	{
 		InitializeComponent();
 	}
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        LoadThemePreference();
+    }
+
+    private void TeemaToggled(object sender, ToggledEventArgs e)
+    {
+        bool isDarkMode = e.Value;
+        Application.Current.UserAppTheme = isDarkMode ? AppTheme.Dark : AppTheme.Light;
+        Preferences.Set("AppTheme", isDarkMode ? "Dark" : "Light");
+
+        if (TeemaSwitch != null)
+        {
+            TeemaSwitch.OnColor = isDarkMode ? Colors.Red : Colors.White;
+            TeemaSwitch.ThumbColor = isDarkMode ? Colors.White : Colors.Red;
+
+        }
+    }
+
+    private void LoadThemePreference()
+    {
+        string savedTheme = Preferences.Get("AppTheme", "Light");
+        bool isDarkMode = savedTheme == "Dark";
+
+        Application.Current.UserAppTheme = isDarkMode ? AppTheme.Dark : AppTheme.Light;
+
+        if (TeemaSwitch != null)
+        {
+            TeemaSwitch.IsToggled = isDarkMode; // Asetetaan kytkin oikeaan asentoon
+        }
+    }
 }
--- obj/Debug/net9.0-maccatalyst/maccatalyst-x64/SportEventsApp.AssemblyInfo.cs
+++ obj/Debug/net9.0-maccatalyst/maccatalyst-x64/SportEventsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportEventsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7977761ad5386385390045808f5e4a7d5521f9ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7467786ba226d418c40e26e6ed1bcaf122e63cf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportEventsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportEventsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
